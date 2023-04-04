5116f2de50150d336e03e61a7d5f03a6fa3ad28e
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2023-04-04 23:48:28 +0200

    Create utility.h

---
+++ Week3/utility.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <string.h>
+
+void printMatrix(const double*, const int, const int);
+
+void init_row_counts(int *, const int, const int, const int, const int);
+
+void init_displ_B(int *, const int *, const int , const int );
+
+void init_elem_counts(int *, const int *, const int, const int);
+
+void init_displ_B_col(int *, const int *, const int);
+
+void init_A(double *, const int, const int, const int, const int);
+
+void init_B(double *, const int, const int, const int, const int);
+
+void matrixMultiply(double *, const double *, const double *, const int *,
+                    const int *, const int, const int, const int);
+
+void MPICalls(const int *, const int *, int *, int *, const double *, double *,
+              const int, const int, const int, const int);
