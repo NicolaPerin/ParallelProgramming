81f07d9c42dd3f944af2f2892544db149f1bf1de
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2023-04-26 14:04:31 +0200

    Update Obi_Wan_Jacobi.c

--- Week4/Solution/Obi_Wan_Jacobi.c
+++ Week4/Solution/Obi_Wan_Jacobi.c
@@ -25,21 +25,15 @@ int main(int argc, char* argv[]) {
   int n_loc = (N + 2) / wsz; // 11 / 3 = 3
   int rest = (N + 2) % wsz; // 11 % 3 = 2
   if (rank == 0) printf("n_loc: %d rest: %d\n", n_loc, rest);
-  int* ghost_counts = (int *) malloc( wsz * sizeof(int) );
-  int* recv_counts = (int *) malloc( wsz * sizeof(int) );
-  int* displ = (int *) malloc( wsz * sizeof(int) );
 
-  // If rank < rest, the process gets (N + 2) / wsz + 3 rows, otherwise, + 2
-  for (int i = 0; i < wsz; i++) { ghost_counts[i] = (i < rest) ? n_loc + 3 : n_loc + 2; }
-  for (int i = 0; i < wsz; i++) recv_counts[i] = ghost_counts[i] - 2;
-  ghost_counts[0] -= 1; ghost_counts[wsz - 1] -= 1; // first and last processes only have 1 ghost rows
-  displ[0] = 0;
-  for (int i = 1; i <= wsz; i++) displ[i] = displ[i-1] + recv_counts[i-1];
+  int* rows = (int *) malloc( wsz * sizeof(int) ); // N° of actual rows
+  int* offset = (int *) malloc( wsz * sizeof(int) ); // For initialization
+
+  initCounts(n_loc, wsz, rest, rows, offset);
 
   if (rank == 0) {
-    printf("matrix size: (int) %d (ext) %d\n", N, N + 2);
-    printf("ghost_counts, recv_counts, displacements:\n");
-    for (int i = 0; i < wsz; i++) printf("%d %d %d\n", ghost_counts[i], recv_counts[i], displ[i]);
+    printf("N (int): %d\nN (ext): %d\nn_loc: %d\nrest: %d\n\n", N, N + 2, n_loc, rest);
+    for (int i = 0; i < wsz; i++) printf("%d %d\n", rows[i], offset[i]);
     printf("\nnumber of iterations = %d\n", iterations);
     printf("element for checking = Mat[%d,%d]\n",row_peek, col_peek);
   }
@@ -50,31 +44,44 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  matrix = (double *) malloc( ghost_counts[rank] * (N + 2) * sizeof(double) );
-  matrix_new = (double *) malloc( ghost_counts[rank] * (N + 2) * sizeof(double) );
-  memset( matrix, 0, ghost_counts[rank] * (N + 2) * sizeof(double) );
-  memset( matrix_new, 0, ghost_counts[rank] * (N + 2) * sizeof(double) );
+  // allocation includes space for ghost rows
+  matrix = (double*)malloc( (rows[rank] + 2) * (N + 2) * sizeof(double) );
+  matrix_new = (double*)malloc( (rows[rank] + 2) * (N + 2) * sizeof(double) );
 
-  initMatrix(rank, wsz, N, ghost_counts, displ, matrix);
-  initMatrix(rank, wsz, N, ghost_counts, displ, matrix_new);
+  initMatrix(rank, wsz, N, rows, offset, matrix);
+  initMatrix(rank, wsz, N, rows, offset, matrix_new);
 
-/*
+  // Exchange ghost rows
+  int above = (rank == 0) ? MPI_PROC_NULL : rank - 1; // First process doesn't send its upper row to anybody
+  int below = (rank == wsz - 1) ? MPI_PROC_NULL : rank + 1; // Last process doesn't sent its lower row to anybody
+
   t_start = seconds();  // start algorithm
   for (int it = 0; it < iterations; it++) {
-    evolve( matrix, matrix_new, N );
-    // swap the pointers // very clever!
+    // Exchange upper row
+    MPI_Sendrecv(matrix + N + 2, N + 2, // Send the first (actual) row (the second)
+                 MPI_DOUBLE, above, 0, // to the process above
+                 matrix + (rows[rank] + 1 ) * (N + 2), N + 2, // Receive it and put it in the lower ghost row (last row)
+                 MPI_DOUBLE, below, 0, // from the process below (same tag)
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // Exchange lower row
+    MPI_Sendrecv(matrix + rows[rank] * (N + 2), N + 2, // Send the last (actual) row (the second to last)
+                 MPI_DOUBLE, below, 0, // to the process below with tag = rank + wsz + 1
+                 matrix, N + 2, // Receive the row and put it in the upper ghost row (the first row)
+                 MPI_DOUBLE, above, 0, // from the process above (same tag)
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    evolve( matrix, matrix_new, rows, rank, N );
+    // swap the pointers
     tmp_matrix = matrix;
     matrix = matrix_new;
     matrix_new = tmp_matrix;
   }
   t_end = seconds();
   
-  printf( "\nelapsed time = %f seconds\n", t_end - t_start );
-  printf( "\nmatrix[%d,%d] = %f\n", row_peek, col_peek, matrix[ ( row_peek + 1 ) * ( N + 2 ) + ( col_peek + 1 ) ] );
-  save_gnuplot( matrix, N );
-*/
+  if (rank == 0) printf( "\nelapsed time = %f seconds\n", t_end - t_start );
+  //printf( "\nmatrix[%d,%d] = %f\n", row_peek, col_peek, matrix[ ( row_peek + 1 ) * ( N + 2 ) + ( col_peek + 1 ) ] );
+  //save_gnuplot( matrix, N );
 
-  printCalls(wsz, rank, N, recv_counts, matrix); // Send to process zero to be printed
+  printCalls(wsz, rank, N, rows, matrix); // Send to process zero to be printed
 
   free( matrix );
   free( matrix_new );
