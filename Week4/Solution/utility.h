6fe335ea8648bb46abfc3caa71207d19b124b081
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2023-05-04 18:54:45 +0200

    Update utility.h

--- Week4/Solution/utility.h
+++ Week4/Solution/utility.h
@@ -9,20 +9,20 @@
 #include <openacc.h>
 #endif
 
-void reset( void ); void red( void ); void green( void ); void yellow( void );
+void reset( void ); void red( void ); void green( void ); void yellow( void ); // Colors
 
-void save_gnuplot(double *, const int, const int, const int, const int*, const int*); // save matrix to file
+void save_gnuplot(double *, const int, const int, const int, const int*, const int*); // Save matrix to file
 
 void exchangeRows(double*, const int*, const int, const int, const int, const int); // MPI_Sendrecv()
 
-void evolve(const double*, double*, const int*, const int, const int); // evolve Jacobi
+void evolve(double*, double*, const int*, const int, const int, const int); // Grid update
 
-void Jacobi(double*, double*, const int*, const int, const int, const int, const int, double);
+void Jacobi(double*, double*, const int*, const int, const int, const int, const int, double*); // Simulation
 
-void initCounts(const int, const int, const int, int*, int*); // determine nr of rows and offset
+void initCounts(const int, const int, const int, int*, int*); // Determine nr of rows and offset
 
-void initMatrix(const int, const int, const int, const int*, const int*, double*); // initial conditions
+void initMatrix(const int, const int, const int, const int*, const int*, double*); // Initial conditions
 
-void printMatrix(const double*, const int, const int); // print a matrix
+void printMatrix(const double*, const int, const int); // Print a matrix
 
-void printCalls(const int, const int, const int, const int*, double*); // send to rank 0 in order and write to terminal
+void printCalls(const int, const int, const int, const int*, double*); // Send to rank 0 in order and write to terminal
