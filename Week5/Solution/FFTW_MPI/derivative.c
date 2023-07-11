ec794dad722ca62b6567fa53bf61b263ad83b271
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2023-07-11 11:28:35 +0200

    Update derivative.c

--- Week5/Solution/FFTW_MPI/derivative.c
+++ Week5/Solution/FFTW_MPI/derivative.c
@@ -1,29 +1,12 @@
-/* Assignment 
- *
- * Adapt this routine to handle distributed arrays for data and deriv
- *
- * Created by G.P. Brandino, I. Girotto, R. Gebauer
- * Last revision: March 2016
- *
- */
-
 #include <stdbool.h>
 #include "utilities.h"
 
-/*
- * Calculate the derivative in direction ipol of the array 'data'
- */
-void derivative( fftw_mpi_handler* fft, int n1, int n2, int n3, double L1, double L2, double L3, int ipol, double* data, double* deriv ){
+void derivative( fftw_mpi_handler* fft, int n1, int n2, int n3, double L1, double L2, double L3, int ipol, double* data, double* deriv ) {
 
     fftw_complex *aux;
     double G;
     int i, i1, i2, i3, index;
 
-    /* 
-     * This implementation will perform
-     * a full 3D FFT of the data, and then derive.
-     *
-     */
     aux = ( fftw_complex* ) fftw_malloc( fft->local_size_grid * sizeof(fftw_complex) );
 
     // First get the FFT of data
